334b214331f4eb67b3d4773165e0a4d7c9b9bc90
Jesuan Goalhath <jesuanprincdd@gmail.com>
2021-10-21 20:13:42 -0300

    Inserindo uma função de temporizador, setTimerout na rocha

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,28 @@
-function sorry(){
-    var userName = prompt("Qual o seu nome?");
 
-    if(userName) {
-        alert("Prazer em conhecer Você, "+userName + ",");
-        document.getElementById("rockImg").src ="images/rock_happy.png";
-        document.getElementById("texto").innerHTML="Sorria você Também!";
-   }
-}
+    var userName;
+
+    function greetUser () {
+        alert("Olá, eu sou sua rocha de estimação");
+    } 
+
+    function touchRock () {
+        if (userName) {
+            alert("Eu gosto de atenção, "+ userName +". Obrigado");
+        }
+        else {
+            userName = prompt("Qual o seu nome?", "Digite seu nome aqui.");
+            if (userName) {
+                alert("Bom conhecer você,"+ userName +".");
+            }
+            document.getElementById("rockImg").src ="images/rock_happy.png";
+            setTimeout("document.getElementById('rockImg').src ='images/rock.png';",
+                1*60*1000);
+
+
+        }
+    }
+
+
+
+
+
